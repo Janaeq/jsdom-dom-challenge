a2c3f328cd94b9c5bc164dd4b791c0cdee36147b
Janae Quinones <janaemassa@gmail.com>
2021-03-19 19:40:06 -0400

    commit tree a49b246ce3517d805cf38bf9f38d517f31b18a33

---
+++ js/challenge.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const counter = document.querySelector("#counter");
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const heart = document.querySelector("#heart");
+const pause = document.querySelector("#pause");
+const likes = document.querySelector(".likes");
+const form = document.querySelector("#comment-form");
+const inputField = document.querySelector("#comment-input");
+const commentDiv = document.querySelector("#list");
+
+
+const count = () => (counter.innerText = parseInt(counter.innerText) + 1);
+
+plus.addEventListener("click", count);
+minus.addEventListener("click", () => counter.innerText--);
+pause.addEventListener("click", () => {
+  if (counter.classList.contains("paused")) {
+    paused();
+  } else {
+    resumed();
+  }
+  toggleButtons();
+});
+
+function paused() {
+  counter.classList.remove("paused");
+  pause.innerText = "pause";
+  timer = setInterval(count, 1000);
+}
+
+function resumed() {
+  counter.classList.add("paused");
+  pause.innerText = "resume";
+  clearInterval(timer);
+}
+
+function toggleButtons() {
+  document.querySelectorAll("button").forEach((button) => {
+    if (button.id !== "pause") {
+      button.disabled = !button.disabled;
+    }
+  });
+}
+
+let timer = setInterval(count, 1000);
+
+let hash = {}; 
+heart.addEventListener("click", function () {
+  let number = counter.innerHTML;
+  console.log(hash);
+  if (hash[number]) hash[number]++;
+  else hash[number] = 1;
+  commentDiv.insertAdjacentHTML(
+    "beforeend",
+    `<li>${counter.innerHTML} has ${hash[number]} like(s)!</li>`
+  );
+});
+
+function addComment(comment) {
+  commentDiv.insertAdjacentHTML("beforeend", `<li>${comment}</li>`);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addComment(e.target.comment.value);
+  form.reset();
+});
